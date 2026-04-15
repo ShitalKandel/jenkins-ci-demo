acc2f645344332caeb0f0e6e3aa52f2a79f6c1a6
ShitalKandel <kandelshittal@gmail.com>
2026-04-15 18:58:42 +0545

    fix: delibrate bug on test.sh for lab purpose

--- test.sh
+++ test.sh
@@ -3,10 +3,13 @@
 
 echo 'Running tests...'
 ./app.sh
-if [ $? -eq 0 ]; then
-    echo 'TEST PASSED: app.sh ran successfully'
-    exit 0
-else
-    echo 'TEST FAILED'
-    exit 1
-fi
+echo "failing this test for lab purpose."
+exit 1
+
+#if [ $? -eq 0 ]; then
+ #   echo 'TEST PASSED: app.sh ran successfully'
+  #  exit 0
+#else
+ #   echo 'TEST FAILED'
+  #  exit 1
+#fi
